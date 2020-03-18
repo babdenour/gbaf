8dee921f949b539d29e98883c4de086e91412921
Abde <abdenour.bensouna@rocketstud.io>
2020-03-18 22:07:00 +0100

    change place of holder

--- contact.php
+++ contact.php
@@ -28,13 +28,13 @@ session_start();
             <nav class="navbar navbar-white bg-white justify-content-center">
                 <ul class="nav justify-content-center">
                     <li class="nav-item">
-                        <p class="nav-link">GBAF par Abdenour Bensouna</p>  
-                    </li>
-                    <li class="nav-item">
                         <a class="nav-link" href="./index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./mentions_legales.php">Mentions Légales</a>
+                    </li>
+                    <li class="nav-item">
+                        <p class="nav-link">GBAF par Abdenour Bensouna</p>  
                     </li>
                 </ul>
             </nav>
